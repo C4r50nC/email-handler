73a83d8c769060e46c6a3a6e927d99d4cd6d05aa
C4r50nC <xichen059@gmail.com>
2024-03-25 20:15:00 +1100

    feat: implement inbox verification for background script

--- background.js
+++ background.js
@@ -3,12 +3,24 @@ let currentURL = "";
 // Function to check if the current URL matches the Gmail URL
 function checkURL(currentURL) {
   // Define the pattern for Gmail URLs
-  const gmailURLPattern = /^https:\/\/mail\.google\.com\/mail\/u\/\d+\/.*#inbox\/[A-Za-z0-9]+$/; 
+  const gmailURLPattern =
+    /^https:\/\/mail\.google\.com\/mail\/u\/\d+\/.*#inbox\/[A-Za-z0-9]+$/;
   // Check if the current URL matches the pattern
   return gmailURLPattern.test(currentURL);
 }
 
 chrome.runtime.onMessage.addListener((_msg, sender, _sendResponse) => {
   currentURL = sender.url;
+  let isInbox = checkURL(currentURL);
+  if (isInbox) {
+    console.log("Inbox!");
+  }
 });
 
+chrome.tabs.onUpdated.addListener((_tabId, changeInfo, _tab) => {
+  const currentURL = changeInfo.url;
+  let isInbox = checkURL(currentURL);
+  if (isInbox) {
+    console.log("Inbox!");
+  }
+});
